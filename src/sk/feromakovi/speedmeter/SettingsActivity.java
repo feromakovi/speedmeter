61d192e26641cc40d7d14d85cbefa80fce386b91
Richard Samela <feromakovi@gmail.com>
2013-12-19 18:09:56 +0100

    Create preference for speed view size, stores and restores percentage value but actually it's not working with text view size

--- src/sk/feromakovi/speedmeter/SettingsActivity.java
+++ src/sk/feromakovi/speedmeter/SettingsActivity.java
@@ -5,19 +5,22 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.DialogPreference;
 import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
 
 public class SettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener{
 	
 	private ListPreference mUnitsPreference;
+	private DialogPreference mSpeedSizePreference;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.settings);
 		mUnitsPreference = (ListPreference) findPreference("units_system");
-		mUnitsPreference.setSummary(mUnitsPreference.getEntries()[mUnitsPreference.findIndexOfValue(mUnitsPreference.getValue())]);
+		mSpeedSizePreference = (DialogPreference) findPreference("speed_view_size");
+		refreshSummaries();
 	}	
 	
 	@Override
@@ -34,10 +37,15 @@ public class SettingsActivity extends PreferenceActivity implements OnSharedPref
 
 	@Override
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-		mUnitsPreference.setSummary(mUnitsPreference.getEntries()[mUnitsPreference.findIndexOfValue(mUnitsPreference.getValue())]);
+		refreshSummaries();
 	}
 	
 	public static Intent getIntent(Context context){
 		return new Intent(context, SettingsActivity.class);
+	}
+	
+	private void refreshSummaries(){
+		mUnitsPreference.setSummary(mUnitsPreference.getEntries()[mUnitsPreference.findIndexOfValue(mUnitsPreference.getValue())]);
+		this.mSpeedSizePreference.setSummary(getString(R.string.preference_text_secondary_speed_view_size, getPreferenceScreen().getSharedPreferences().getInt("speed_view_size", 100)));
 	}
 }
---
+++ src/sk/feromakovi/speedmeter/view/SeekBarPreference.java
@@ -0,0 +1,76 @@
+package sk.feromakovi.speedmeter.view;
+
+import sk.feromakovi.speedmeter.R;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+public class SeekBarPreference extends DialogPreference implements OnSeekBarChangeListener {
+
+	private static final String TAG = "SpeedMeter - " + SeekBarPreference.class.getSimpleName();
+	private static final String KEY = "speed_view_size";
+
+	private SeekBar mSeekBar;
+	private TextView mStatusText;
+
+	public SeekBarPreference(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		initPreference(context, attrs);
+	}
+
+	public SeekBarPreference(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		initPreference(context, attrs);
+	}
+
+	private void initPreference(Context context, AttributeSet attrs) {
+		setPersistent(false);
+	}
+
+	@Override
+	protected View onCreateDialogView() {
+		LayoutInflater mInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View root =  mInflater.inflate(R.layout.preference_seek_bar, null, false);
+		this.mSeekBar = (SeekBar) root.findViewById(R.id.Dialog_SeekBarPreference_seekbar);
+		this.mStatusText = (TextView) root.findViewById(R.id.Dialog_SeekBarPreference_percentage);
+		this.mSeekBar.setOnSeekBarChangeListener(this);
+		return root;
+	}
+	
+	@Override
+	protected void onBindDialogView(View view) {
+		super.onBindDialogView(view);
+		SharedPreferences sharedPreferences = getSharedPreferences();
+		int value = sharedPreferences.getInt(KEY, 100);
+		this.mStatusText.setText(value + "%");
+		this.mSeekBar.setProgress(value);
+	}
+	
+	@Override
+	protected void onDialogClosed(boolean positiveResult) {
+		super.onDialogClosed(positiveResult);
+		if (positiveResult) {
+	        Editor editor = getEditor();
+	        editor.putInt(KEY, this.mSeekBar.getProgress());
+	        editor.commit();
+	    }
+	}
+
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+		this.mStatusText.setText(progress + "%");
+	}
+
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {}
+}
